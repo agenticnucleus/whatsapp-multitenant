70ff3a2eba0cee42b83b1a88545ae132f4e39448
jonnathanypg <jonnathan.ypg@gmail.com>
2026-03-02 21:03:13 -0500

    chore: initial release v1.0.0

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// Core exports
+export { BaileysTransporter } from './infrastructure/repositories/baileys.repository';
+export { LeadCreate } from './application/lead.create';
+export { ContainerBuilder } from 'node-dependency-injection';
+export { default as container } from './infrastructure/ioc';
+
