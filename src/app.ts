1ae57c9d255656b6fcbb4cbe82c6cb1a86d4c968
jonnathanypg <jonnathan.ypg@gmail.com>
2026-03-03 21:57:03 -0500

    Mejora de libreria para hacerlo un Plug & Play

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import "dotenv/config"
 import express from "express"
 import cors from "cors"
@@ -7,6 +8,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.static('tmp'))
-app.use(`/`,routes)
+app.use(`/`, routes)
 
 app.listen(port, () => console.log(`Ready...${port}`))
